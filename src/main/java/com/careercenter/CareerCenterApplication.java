e8a46b360fba84e4d7c4d1c0a4a88248a97625b8
Oz <ozcey2018@gmail.com>
2020-11-19 20:06:17 -0500

    created spring boot project

---
+++ src/main/java/com/careercenter/CareerCenterApplication.java
@@ -0,0 +1,13 @@
+package com.careercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careercenter/CareerCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
