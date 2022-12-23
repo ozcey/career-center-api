4da7bd77a46342a1180ec6c930d836201983cf7d
Oz <ozcancey20@gmail.com>
2022-12-22 23:03:01 -0500

    updated docker configs

--- src/main/java/com/careercenter/CareerCenterApplication.java
+++ src/main/java/com/careercenter/CareerCenterApplication.java
@@ -1,13 +1,27 @@
 package com.careercenter;
 
+import com.careercenter.entities.Role;
+import com.careercenter.model.RoleName;
+import com.careercenter.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class CareerCenterApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(CareerCenterApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner saveRoles(RoleRepository roleRepository) {
+		return args -> {
+			roleRepository.save(new Role(1L, RoleName.ROLE_ADMIN));
+			roleRepository.save(new Role(2L, RoleName.ROLE_USER));
+		};
 	}
 
 }
