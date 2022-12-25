de509a254cf3b88236955ff53fca663d63df162a
Oz <ozcancey20@gmail.com>
2022-12-25 14:46:23 -0500

    fixed app context failed error

--- src/test/java/com/careercenter/CareerCenterApplicationTests.java
+++ src/test/java/com/careercenter/CareerCenterApplicationTests.java
@@ -3,7 +3,7 @@ package com.careercenter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CareerCenterApplicationTests.class)
 class CareerCenterApplicationTests {
 
 	@Test
