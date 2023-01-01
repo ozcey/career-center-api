584bcf00a8612bfc1dedc67bff8671a80f7cd7b3
Oz <ozcancey20@gmail.com>
2023-01-01 11:18:28 -0500

    updated open api config

--- src/main/java/com/careercenter/CareerCenterApplication.java
+++ src/main/java/com/careercenter/CareerCenterApplication.java
@@ -1,9 +1,12 @@
 package com.careercenter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class CareerCenterApplication {
 
 	public static void main(String[] args) {
